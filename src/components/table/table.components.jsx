2234287d12b572f28f8c6a3c30128597dc4b8516
Kohsaku <kosakuhayashi@gmail.com>
2021-01-19 00:14:32 +0900

    src/components

---
+++ src/components/table/table.components.jsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+//
+//
+// const Table = () => (
+//   <div>
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Currency</th>
+//           <th>Rate</th>
+//         </tr>
+//       </thead>
+//         <tr>
+//           <td>JPY</td>
+//           <td>¥ 1</td>
+//         </tr>
+//         <tr>
+//           <td>USD</td>
+//           <td>¥ 100</td>
+//         </tr>
+//         <tr>
+//           <td>EUR</td>
+//           <td>¥ 120</td>
+//         </tr>
+//     </table>
+//   </div>
+// );
+//
+// export default Table;
