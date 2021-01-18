14f12618845c8f68502403b6d7752b5520ccfd48
Kohsaku <kosakuhayashi@gmail.com>
2021-01-19 00:11:46 +0900

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React from 'react';
+import Table from './components/table/table.components';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      rates: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.exchangeratesapi.io/latest?base=USD")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            rates: result.rates
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  createTable = () => {
+    const rates = this.state;
+    var ratesArr = Object.keys(rates).map(i => rates[i])[2];
+    var table = [];
+    var children = [];
+    var displayedCurrencies = ["JPY", "USD", "EUR", "HKD"];
+
+    for (var key in ratesArr) {
+      if (ratesArr.hasOwnProperty(key) && displayedCurrencies.includes(key)) {
+        children.push(
+          <tr>
+          <td>{key}</td>
+          <td>{ratesArr[key]}</td>
+          </tr>
+        );
+      }
+    }
+  table.push(<tbody>{children}</tbody>);
+
+  return table;
+};
+
+  render() {
+    const { error, isLoaded } = this.state;
+
+    if (error) {
+      return <div>Oops: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Currency</th>
+                  <th>Rate</th>
+                </tr>
+              </thead>
+              {this.createTable()}
+            </table>
+          </div>
+        </div>
+      )
+    }
+  }
+}
 export default App;
